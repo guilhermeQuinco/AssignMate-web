83275bfe05843b79cd02db4f494700df2b3d0438
Yasmina <yasmina.andrade@yahoo.com.br>
2025-04-14 17:51:26 -0400

    corrigindo erro no cadastro de professor

--- src/app/dashboard/usuarios/professores/novo/page.tsx
+++ src/app/dashboard/usuarios/professores/novo/page.tsx
@@ -6,9 +6,10 @@ import { getProfessors } from "../actions/professors";
 import ProfessorForm from "./components/professor-form";
 
 const NewProfessor = async () => {
-  const response = await getProfessors();
-
-  const lastRegistration = response
+  const response = await getProfessors(); // ou const { data } = await getProfessors();
+  console.log("Response:", response);
+  const arrayData = Array.isArray(response) ? response : response.data;
+  const lastRegistration = arrayData
     .map((registration: { matricula: string }) => registration.matricula)
     .sort()
     .at(-1);
