941dd00720aa8b1631dcd10e52fdc1402fea116c
RaianT <raiantomaz@gmail.com>
2025-05-02 14:02:46 -0300

    Fix: Adicionado UTC na DateFormatter

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,10 @@
-export function DateFormatter(date: string) {
-  return new Date(date).toLocaleDateString("pt-br", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
+export function DateFormatter(date: string | Date) {
+  const d = typeof date === "string" ? new Date(date) : date;
+
+  // Pegando a data como UTC
+  const day = d.getUTCDate().toString().padStart(2, "0");
+  const month = (d.getUTCMonth() + 1).toString().padStart(2, "0");
+  const year = d.getUTCFullYear();
+
+  return `${day}/${month}/${year}`;
 }
